fdd2b06d6ced2ae228742e5a68722c726c245f90
Vinicius Santos <dukeofgod@outlook.com>
2022-12-02 09:23:53 -0300

    fix: validate

--- src/helpers/findColorsByResistance.ts
+++ src/helpers/findColorsByResistance.ts
@@ -3,19 +3,24 @@ import { formatColorValue } from './formatColorValue';
 
 export function findColorsByResistence(resistence: string) {
   try {
+    if(resistence.charAt(0) !== "0"){
+      
       const resistenceWithoutSpaces = resistence.replace(/\s/g, '');
       const value = [];
       let teste = resistenceWithoutSpaces.slice(2);
-
+      
       value.push(numberBands.get(resistenceWithoutSpaces[0]));
       value.push(numberBands.get(resistenceWithoutSpaces[1]));
       value.push(numberBands.get(teste.length.toString()));
-
+      
       const formart = formatColorValue(value as Array<string>);
-
+      
       return formart;
-    
-  } catch (error) {
+    }else{
+      return "Valor Invalido"
+    }
+      
+    } catch (error) {
     return 'Ocorreu algum erro no servidor, tente novamente mais tarde';
   }
 }
--- src/helpers/findResistanceByColor.ts
+++ src/helpers/findResistanceByColor.ts
@@ -4,25 +4,30 @@ import { formatResistorValue } from './formatResistorValue';
 
 export function findResistanceByColor({ color1, color2, color3 }: IResistor) {
   try {
-    const color1WithoutSpaces = color1.replace(/\s/g, '');
-    const color2WithoutSpaces = color2.replace(/\s/g, '');
-    const color3WithoutSpaces = color3.replace(/\s/g, '');
+    if(color1.toLowerCase() === "preto"){
 
-    let value = '';
-
-    value += colorsBands.get(color1WithoutSpaces.toLowerCase());
-    value += colorsBands.get(color2WithoutSpaces.toLowerCase());
-
-    const number = colorsBands.get(color3WithoutSpaces.toLowerCase());
-
-    for (let i = 0; i < (number === undefined ? 0 : number); i += 1) {
-      value += 0;
+      const color1WithoutSpaces = color1.replace(/\s/g, '');
+      const color2WithoutSpaces = color2.replace(/\s/g, '');
+      const color3WithoutSpaces = color3.replace(/\s/g, '');
+      
+      let value = '';
+      
+      value += colorsBands.get(color1WithoutSpaces.toLowerCase());
+      value += colorsBands.get(color2WithoutSpaces.toLowerCase());
+      
+      const number = colorsBands.get(color3WithoutSpaces.toLowerCase());
+      
+      for (let i = 0; i < (number === undefined ? 0 : number); i += 1) {
+        value += 0;
+      }
+      
+      const formart = formatResistorValue(value);
+      
+      return formart;
+    }else{
+      return "Valor Invalido"
     }
-
-    const formart = formatResistorValue(value);
-
-    return formart;
-  } catch (error) {
-    return 'Ocorreu algum erro no servidor, tente novamente mais tarde';
+    } catch (error) {
+      return 'Ocorreu algum erro no servidor, tente novamente mais tarde';
   }
 }
